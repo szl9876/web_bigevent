7c12f11bb118a84efd7625842b21567fbe99dcb4
szl <szl9876@163.com>
2021-09-04 16:56:43 +0800

    完成了首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,6 +5,28 @@
 
 $.ajaxPrefilter(function (options) {
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-    console.log(options.url);
-    
+    // console.log(options.url);
+
+    //统一为有权限的接口  设置headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+
+    }
+
+
+    //全局统一挂载 complete回调函数
+    options.complete = function (res) {
+        console.log(res);
+        // 在complete回调函数中 可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1.清除本地存储的token
+            localStorage.removeItem('token')
+
+            //2.重新跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
+
 })
